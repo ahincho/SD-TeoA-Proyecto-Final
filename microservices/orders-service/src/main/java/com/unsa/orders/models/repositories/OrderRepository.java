7902f6b2f692d8326f09c6e734236cda6075c17f
ahincho <ahincho@unsa.edu.pe>
2023-07-27 23:38:19 -0500

    Creating the Data Repository using JPA

---
+++ microservices/orders-service/src/main/java/com/unsa/orders/models/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.unsa.orders.models.repositories;
+
+import com.unsa.orders.models.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
