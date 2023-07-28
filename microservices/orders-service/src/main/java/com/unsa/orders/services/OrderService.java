a98ef3be624638bd3dd28e7941b45ca7af58171f
ahincho <ahincho@unsa.edu.pe>
2023-07-28 00:01:36 -0500

    Sending a Request to Inventory Endpoint to Check the Stock

--- microservices/orders-service/src/main/java/com/unsa/orders/services/OrderService.java
+++ microservices/orders-service/src/main/java/com/unsa/orders/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.unsa.orders.services;
 
+import com.unsa.orders.models.dtos.BaseResponse;
 import com.unsa.orders.models.dtos.OrderItemRequest;
 import com.unsa.orders.models.dtos.OrderRequest;
 import com.unsa.orders.models.entities.Order;
@@ -7,7 +8,7 @@ import com.unsa.orders.models.entities.OrderItems;
 import com.unsa.orders.models.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.reactive.function.client.WebClient;
 import java.util.UUID;
 
 @Service
@@ -15,16 +16,28 @@ public class OrderService {
 
     @Autowired
     OrderRepository orderRepository;
+    WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
-        // Check for Inventory
-        Order order = new Order();
-        order.setCode(UUID.randomUUID().toString());
-        order.setOrderItems(orderRequest.getOrderItems().stream()
-                .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
-                .toList()
-        );
-        orderRepository.save(order);
+        // Check for Inventory, We need to use a HTTP Client to consume Inventory API
+        BaseResponse result = webClientBuilder.build()
+                .post()
+                .uri("http://localhost:8085/api/inventory/in-stock")
+                .bodyValue(orderRequest.getOrderItems())
+                .retrieve()
+                .bodyToMono(BaseResponse.class)
+                .block();
+        if (result != null && !result.hasErrors()) {
+            Order order = new Order();
+            order.setCode(UUID.randomUUID().toString());
+            order.setOrderItems(orderRequest.getOrderItems().stream()
+                    .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
+                    .toList()
+            );
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Some of the products are not in stock.");
+        }
     }
 
     private OrderItems mapOrderItemRequestToOrderItem(OrderItemRequest orderItemRequest, Order order) {
