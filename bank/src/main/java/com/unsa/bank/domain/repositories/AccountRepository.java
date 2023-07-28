9d9a7b4875a97d854db984deedadcade8eb6a0c6
ahincho <ahincho@unsa.edu.pe>
2023-07-28 13:58:43 -0500

    Create the Account Repository with JPA

---
+++ bank/src/main/java/com/unsa/bank/domain/repositories/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.unsa.bank.domain.repositories;
+
+import com.unsa.bank.domain.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
