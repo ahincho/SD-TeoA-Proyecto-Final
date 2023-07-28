d004ab2e5e2f929ef888fe31e8e2d350ff56041d
ahincho <ahincho@unsa.edu.pe>
2023-07-27 19:20:40 -0500

    Creating User Class with Lombok and JPA Entity

---
+++ bank/src/main/java/com/unsa/bank/domain/entities/User.java
@@ -0,0 +1,24 @@
+package com.unsa.bank.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product")
+@Setter @Getter @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", firstname='" + firstname + "', lastname='" + lastname + "']";
+    }
+
+}
