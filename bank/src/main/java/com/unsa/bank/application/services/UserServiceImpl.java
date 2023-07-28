31cbe89bb5934a542eba563dd0462422fe097da3
ahincho <ahincho@unsa.edu.pe>
2023-07-27 19:47:31 -0500

    Creating the User Service to Communicate with the Database

---
+++ bank/src/main/java/com/unsa/bank/application/services/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package com.unsa.bank.application.services;
+
+import com.unsa.bank.application.ports.UserService;
+import com.unsa.bank.domain.entities.User;
+import com.unsa.bank.domain.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            return null;
+        }
+        User userToUpdate = optionalUser.get();
+        userToUpdate.setFirstname(user.getFirstname());
+        userToUpdate.setLastname(user.getLastname());
+        return userRepository.save(userToUpdate);
+    }
+
+    @Override
+    public User deleteUser(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            return null;
+        }
+        User userToDelete = optionalUser.get();
+        userRepository.delete(userToDelete);
+        return userToDelete;
+    }
+
+}
