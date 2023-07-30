8de8515f31e93d6036e013ccf07a8f2d97400441
ahincho <ahincho@unsa.edu.pe>
2023-07-30 04:28:16 -0500

    Creating an Exclusive Configuration Package

--- bank-backend/src/main/java/com/unsa/bank/config/CorsConfig.java
+++ bank-backend/src/main/java/com/unsa/bank/config/CorsConfig.java
@@ -1,10 +1,11 @@
-package com.unsa.bank.infraestructure.config;
+package com.unsa.bank.config;
 
-import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.validation.constraints.NotNull;
 
 @Configuration
 public class CorsConfig {
