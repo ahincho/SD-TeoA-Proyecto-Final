1d33e25be2c3b6e2775f410d043f3641c3ec6f5a
ahincho <ahincho@unsa.edu.pe>
2023-07-30 10:33:14 -0500

    Updating NodeJS Version and Other Dependencies

--- bank-frontend/src/App.js
+++ bank-frontend/src/App.js
@@ -8,6 +8,8 @@ function App() {
     <React.StrictMode>
       { /* <Login/> */}
       { /* <Register/> */}
+      <Login/>
+      <Register/>
       <Table bankName = "Arequipa Bank" apiUrl = "http://localhost:8085/api/accounts/u"/>
       <Table bankName = "Cusco Bank" apiUrl = "http://localhost:8086/api/accounts/u"/>
       <Table bankName = "Lima Bank" apiUrl = "http://localhost:8087/api/accounts/u"/>
